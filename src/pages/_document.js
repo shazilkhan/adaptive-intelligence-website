011a433efb9ac900a3ee9f5023df374a156cd837
shazilkhan <shazilh225@gmail.com>
2026-03-04 09:17:22 +0500

    header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
 import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v14-pagesRouter';
 import theme from '@/styles/theme';
-import Script from 'next/script';
 
 export default function AdaptiveDocument(props) {
   return (
@@ -13,9 +12,19 @@ export default function AdaptiveDocument(props) {
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
         <DocumentHeadTags {...props} />
-
-
-
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.smartlook||(function(d) {
+                var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+                var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+                c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+              })(document);
+              smartlook('init', '073912b4016eb2c02e314ca6f518ee7bf35c3ed5', { region: 'eu' });
+            `,
+          }}
+        />
       </Head>
       <body>
 
